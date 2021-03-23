db2f6c5d472a72e2763811dc54ffafc9e4c3dd75
CBKain <65433990+CBKain@users.noreply.github.com>
2021-03-22 21:23:51 -0500

    This is the beginning of a program that just repeats the number that the user was promptly inputted

--- _Scripts/test.py
+++ _Scripts/test.py
@@ -1,3 +1,8 @@
-Greeting = "Hello World!"
+int = userNum
 
-print(Greeting)
+print("This is a test, I'm just gonna repeat the number you inputted.")
+
+print("Enter a number: ")
+userInput = input(userNum)
+
+print("This is your number: " + userInput)
