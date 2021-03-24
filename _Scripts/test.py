52d3473b9af93e2f542741cc3e66636067385fc6
CBKain <65433990+CBKain@users.noreply.github.com>
2021-03-23 21:04:02 -0500

    Debugged the program to make it make sense

--- _Scripts/test.py
+++ _Scripts/test.py
@@ -1,8 +1,6 @@
-int = userNum
-
 print("This is a test, I'm just gonna repeat the number you inputted.")
 
 print("Enter a number: ")
-userInput = input(userNum)
+userNum = input()
 
-print("This is your number: " + userInput)
+print("This is your number: " + userNum)
